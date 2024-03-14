46c799ffabb833f418305186d3f9f3058539d405
oscirt <89642258+oscirt@users.noreply.github.com>
2024-03-14 17:10:58 +0300

    Material order system (#2)
    * Система закупки сырья
    
    * Система истории. Создал сущности
    
    * Система истории. Создал репозиторий историй

---
+++ src/main/java/ru/mts/case_project/entity/history/ClientOrderHistory.java
@@ -0,0 +1,12 @@
+package ru.mts.case_project.entity.history;
+
+public class ClientOrderHistory implements History {
+
+    private long clientOrderId;
+
+    private int orderData;
+
+    private long orderProcessingTime;
+
+    private boolean orderDeliveryFlag;
+}
---
+++ src/main/java/ru/mts/case_project/service/IngredientOrderService.java
@@ -0,0 +1,8 @@
+package ru.mts.case_project.service;
+
+import ru.mts.case_project.entity.IngredientOrder;
+
+public interface IngredientOrderService {
+
+    void process(IngredientOrder ingredientOrder);
+}
