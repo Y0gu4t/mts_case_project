6d4af73943bc02629134ae9d5dcbef25f7a14e53
Yegor <y0gu4t@apx_managed.vanilla>
2024-03-14 15:29:51 +0300

    upload project settings

---
+++ src/main/java/ru/mts/case_project/CaseProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.mts.case_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mts/case_project/CaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mts.case_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
